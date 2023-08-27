e728dc927d7fdeba09f2394ae79fa6d23526b40d
sabbir <mdsabbirrahmanm740@gmail.com>
2023-08-27 12:32:11 +0600

    break and continue

---
+++ break continue loop/main.js
@@ -0,0 +1,23 @@
+const numbers=[1,2,3,4,5,6,7,8,9];
+for(let num of numbers){
+if(num > 4){
+    break;
+    
+}
+console.log(num)
+};
+
+
+const number=[1,-2,3,-4,5,-6,7,8,9];
+for(let num of number){
+if(num < 0){
+    continue;
+    
+}
+console.log(num)
+};
+
+
+
+
+
